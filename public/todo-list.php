af2c58c400773798281c5d38461755ac6ac13116
ryanorsinger <ryan.orsinger@gmail.com>
2014-02-25 11:27:55 -0600

    prompting for file input now.. still need to process the file and add it to my existing todo list

--- public/todo-list.php
+++ public/todo-list.php
@@ -1,15 +1,8 @@
 <?php
 
-	var_dump($_POST);
-	var_dump($_GET);
+
 
 $filename = 'data/todo_list.txt';
-
-// if (filesize($filename) > 0) {
-// $items = open_file($filename);
-// } else {
-// 	$items = ();
-// }
 
 // set the items array to the file contents if the file exists, otherwise make it an empty array
 $items = (filesize($filename) > 0) ? $items = open_file($filename) : array();
@@ -24,7 +17,7 @@ $items = (filesize($filename) > 0) ? $items = open_file($filename) : array();
     		return $items;
     	}
 
-// save an array to file - 1 value per line
+// save an array to file as a flat file with items delimited by a newline
     	function save_file($filename, $items) {
     		$itemStr = implode("\n", $items);
     		$handle = fopen($filename, "w");
@@ -48,7 +41,22 @@ $items = (filesize($filename) > 0) ? $items = open_file($filename) : array();
     		array_splice($items, $_GET['remove'], 1);
 			save_file($filename, $items);
 			header("Location: todo-list.php");
+			die();
     	}
+
+	// Verify there were uploaded files and no errors
+	if (count($_FILES) > 0 && $_FILES['file1']['error'] == 0) {
+    	// Set the destination directory for uploads
+	    $upload_dir = '/vagrant/sites/codeup.dev/public/uploads/';
+	    // Grab the filename from the uploaded file by using basename
+	    $filename = basename($_FILES['file1']['name']);
+	    // Create the saved filename using the file's original name and our upload directory
+	    $saved_filename = $upload_dir . $filename;
+	    // This concatenates the directory with the filename and gives us a saved filename and path.
+	    // Move the file from the temp location to our uploads directory
+	    move_uploaded_file($_FILES['file1']['tmp_name'], $saved_filename);
+	}
+
 
 	?>
 
@@ -62,6 +70,7 @@ $items = (filesize($filename) > 0) ? $items = open_file($filename) : array();
 	</head>
 	<body>
 
+
 <h1> TODO List: The Awesome To Do List of My Great Agenda! </h1>
 		
  
@@ -74,24 +83,40 @@ $items = (filesize($filename) > 0) ? $items = open_file($filename) : array();
 	</ul>
 
 
+	<h3> Enter New To Do Items </h3>
 
-
-	<h2> Enter New To Do Items </h2>
-
-
-
-
-	<form method="POST" action="">
+	<form method="POST" enctype="multipart/form-data" action="">
    		 <p>
       	  <label for="new_todo">New To Do</label>
-     	   <input id="new_todo" name="new_todo" type="text" placeholder="type new todo here">
-  	 	</p>
-  	  		<p>
-       		<input type="submit">
+     	  <input id="new_todo" name="new_todo" type="text" autofocus="autofocus" placeholder="type new todo here">
+       	<br>
+       		<input type="submit" value="Add Item">
+       	
+       	
 		</form>
 
+<h3> Upload a Text File </h3>
+	<?php
+	// 	Check if we saved a file
+	if (isset($saved_filename)) {
+    	// If we did, show a link to the uploaded file
+	    echo "<p>You can download your file <a href='/uploads/{$filename}'>here</a>.</p>";
+	}
+	?>
 
-	
+		<form method="POST" enctype="multipart/form-data">
+    	<p>
+        	<label for="file1">File to upload: </label>
+        	<input type="file" id="file1" name="file1">
+    	</p>
+    	<p>
+        	<input type="submit" value="Upload">
+    </p>
+</form>
+
+
+		<img src="/img/pirate-todo" alt="YARRRR">
+
 
 	</body>
 	
