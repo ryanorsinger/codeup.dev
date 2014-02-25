353699821e5b6400c57efe8647a773f18b3dbb91
ryanorsinger <ryan.orsinger@gmail.com>
2014-02-25 09:33:51 -0600

    todo list ready for production

--- public/todo-list.php
+++ public/todo-list.php
@@ -1,28 +1,15 @@
-<!DOCTYPE html>
-<html>
-	<head>
-        <title>To Do List </title>
-        <meta name="description" content="Ryan Orsinger's To Do list">
-        <meta name="author" content="Ryan Orsinger">
-	</head>
-	<body>
+<?php
 
-<h1> TODO List: The Awesome To Do List of My Great Agenda! </h1>
-
-	<?php 
 	var_dump($_POST);
 	var_dump($_GET);
 
 
-	// load todo.txt
-	// add items from todo.txt to $items array 
+$filename = 'data/todo_list.txt';
 
-	
+$items = open_file($filename);
 
-		$filename = '/vagrant/exercises/data/todo.txt';
-		$items = open_file($filename);
-		
 
+// this reads a file and returns the contents as an array
 		function open_file($filename) {
     		$handle = fopen($filename, "r");
     		$contents = fread($handle, filesize($filename));
@@ -32,6 +19,7 @@
     		return $items;
     	}
 
+// save an array to file - 1 value per line
     	function save_file($filename, $items) {
     		$itemStr = implode("\n", $items);
     		$handle = fopen($filename, "w");
@@ -46,16 +34,30 @@
     		$item = ($_POST['new_todo']);
 			$results = array_push($items, $item);
 			save_file($filename, $items);
+			header("Location: todo-list.php");
     	}
-		
-		if(isset($_GET['remove'])) {
-       		$itemID = ($_GET['remove']);
-			$results = unset($items, $itemID);
+
+
+    	// We use the GET here to remove the item
+		if(!empty($_GET)) {
+    		array_splice($items, $_GET['remove'], 1);
 			save_file($filename, $items);
+			header("Location: todo-list.php");
     	}
 
 	?>
 
+
+<!DOCTYPE html>
+<html>
+	<head>
+        <title>To Do List </title>
+        <meta name="description" content="Ryan Orsinger's To Do list">
+        <meta name="author" content="Ryan Orsinger">
+	</head>
+	<body>
+
+<h1> TODO List: The Awesome To Do List of My Great Agenda! </h1>
 		
  
 	<ul>
@@ -65,11 +67,7 @@
 				<?php echo $item; ?>
 				<a href="?remove=<?php echo $key; ?>">Remove Item;</a></li>	
 				<?php } ?>
-
-
-
 	</ul>
-   
 
 
 
