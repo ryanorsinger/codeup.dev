75439e64fe21028a8d9449557dab168d723924a3
ryanorsinger <ryan.orsinger@gmail.com>
2014-02-27 11:32:26 -0600

    first commit of Object and Class introduction

---
+++ public/object-testing.php
@@ -0,0 +1,49 @@
+<?php
+
+class Conversation {
+
+	// PROPERTY to hold name 
+	public $first_name = '';
+	public $last_name = '';
+
+	// METHOD to say hello to name
+	function say_hello($newline = FALSE)
+	{
+		$greeting = "Hello {$this->first_name} {$this->last_name}";
+		if ($newline == FALSE) {
+			return $greeting;
+		} else {
+			return $greeting . PHP_EOL;
+		}
+	}
+
+	function say_goodbye()
+	{
+		return "Goodbye {$this->first_name} {$this->last_name}";
+	}
+}
+
+// Create a new instance of Conversation
+$chat = new Conversation();
+
+// Set the $name variable
+$chat->first_name = 'Ryan';
+$chat->last_name = 'Orsinger';
+
+// echo out the instance of chat with the firstname property
+//echo $chat->first_name;
+
+
+?>
+
+<html>
+	<head>
+	<title>Title is <?=$chat->say_hello(); ?></title>
+</head>
+<body>
+	<?=$chat->say_hello();?>
+	<hr>
+	<?=$chat->say_goodbye(); ?>
+
+</body>
+</html>
